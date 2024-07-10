624cee0d62e28100799558bcba71e5626ebed0ca
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2024-07-10 14:20:39 +0900

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,10 +1,6 @@
 export const config = {
     text: `
-$[fg.color=f00 **荒らし共栄圏最強**]
-荒らし共栄圏を今すぐ友達に共有
+$[fg.color=f00 **By Amex2189**]
 https://twitter.com/amex2189
-https://xn--68j5e377y.com
-https://xn--68j5e377y.com#Discord
-#荒らし共栄圏
 {{hash}}`
 }
